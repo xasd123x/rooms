93cedb297bf531f8c4ab02276f5e0feeeb8171fe
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-01 20:01:41 +0300

    Update and rename all115522.js to all55.js

--- all55.js
+++ all55.js
@@ -9,6 +9,7 @@ background-image: url(https://up6.cc/2023/11/170009087923123.png); background-si
 
 
 $(`<style>
+.nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r.bord {border-inline: 0px solid #4c121e !important;}
 .nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .mini.u-msg.flex-grow-1 {
     text-align: center;
     margin-top: 53px !important;
@@ -20,23 +21,6 @@ $(`<style>
 	color: transparent !important;
 	background-size: 90% 100%;
 	opacity:0;
-}
-.nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .d-flex.fitimg.u-pic {
-  letter-spacing: 1px;
-  background-size: 108% 108%;
-  -webkit-text-fill-color: white;
-  animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
-}
-.nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .label.label-primary.fa.fa-user.uc {
-  letter-spacing: 1px;
-  -webkit-text-fill-color: #fff;
-  border-radius: 15px 0px 15px 0px !important;
-  height: 27px;
-  margin-top: 40px !important;
-  margin-bottom: 0px !important;
-  background-color: #d9534f00;
-  text-align: left!important;
-  margin-left: 45px!important;
 }
 .nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .c-flex.flex-grow-1.break {
     background-image: url(https://up6.cc/2023/11/170121230754931.gif);
@@ -69,6 +53,23 @@ $(`<style>
   background-size: 100% 100%;
   margin-top: 10px;
   display: none;
+}
+.nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .d-flex.fitimg.u-pic {
+  letter-spacing: 1px;
+  background-size: 108% 108%;
+  -webkit-text-fill-color: white;
+  animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
+}
+.nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .label.label-primary.fa.fa-user.uc {
+  letter-spacing: 1px;
+  -webkit-text-fill-color: #fff;
+  border-radius: 15px 0px 15px 0px !important;
+  height: 27px;
+  margin-top: 40px !important;
+  margin-bottom: 0px !important;
+  background-color: #d9534f00;
+  text-align: left!important;
+  margin-left: 45px!important;
 }
 
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
