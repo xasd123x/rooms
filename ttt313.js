7892c59fe817e8d90cbcad53fb18f7f5e0426052
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-01 23:26:14 +0300

    Update and rename ttt393.js to ttt313.js

--- ttt313.js
+++ ttt313.js
@@ -82,10 +82,3 @@ $(`<style>#mic4 {margin-right: 8px !important;}
     }
 }</style>`).insertBefore('body');
 
-var rid='18c1c14a6c4x52a495a1-xl7kq5r';
-$("style:eq(0)").append(`#d2 .roomh[onclick="rjoin('${rid}')"]{
-background-size: 220% auto;border-radius:  50px !important;border: 1px solid #0000;
-}`);
-$("style:eq(0)").append(`#upro .roomh[onclick="rjoin('${rid}')"]{
-background-image: url(https://up6.cc/2023/11/170009087923123.png); background-size: cover;color: #ffffff;
-}`);
