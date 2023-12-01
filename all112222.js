58f7aaf8904959ae2aa35d0c62d823aa029090a1
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-01 18:39:28 +0300

    Update and rename all11222.js to all112222.js

--- all112222.js
+++ all112222.js
@@ -15,17 +15,6 @@ $(`<style>
   -webkit-text-fill-color: white;
   animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
 }
-.nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .label.label-primary.fa.fa-user.uc {
-  letter-spacing: 1px;
-  -webkit-text-fill-color: #fff;
-  border-radius: 15px 0px 15px 0px !important;
-  height: 27px;
-  margin-top: 40px !important;
-  margin-bottom: 0px !important;
-  background-color: #d9534f00;
-  text-align: left!important;
-  margin-left: 45px!important;
-}
 .nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r.bord {border-inline: 0px solid #4c121e !important;}
 .nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .mini.u-msg.flex-grow-1 {
     text-align: center;
@@ -38,6 +27,17 @@ $(`<style>
 	color: transparent !important;
 	background-size: 90% 100%;
 	opacity:0;
+}
+.nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .label.label-primary.fa.fa-user.uc {
+  letter-spacing: 1px;
+  -webkit-text-fill-color: #fff;
+  border-radius: 15px 0px 15px 0px !important;
+  height: 27px;
+  margin-top: 40px !important;
+  margin-bottom: 0px !important;
+  background-color: #d9534f00;
+  text-align: left!important;
+  margin-left: 45px!important;
 }
 .nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r {
     background-image: url(https://up6.cc/2023/11/170121230754931.gif);
