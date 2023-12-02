06715f3006a52770c0825d9a1973b7b249b508c1
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 01:30:09 +0300

    Update and rename name282.js to name242.js

--- name242.js
+++ name242.js
@@ -1,5 +1,5 @@
 $(`<style>
-.fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o .{
+.fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o {
     border-radius: 0px 15px 15px 15px;
     display: inline-block;
     background-image: url(https://up6.cc/2023/11/170121230754931.gif);
