6227f3a955ae6b9b0bda3415cd2ec846ccfa80ed
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 00:36:43 +0300

    Update and rename name22.js to name212.js

--- name212.js
+++ name212.js
@@ -1,9 +1,9 @@
 $(`<style>
-.fl.hand.nosel.uzr.uhtml.uid18c2b178444x1f034866x1dzh46 .fitimg.u-pic {border-radius: 100px ;border: 2px solid #ffffff;letter-spacing: 1px;background-size: 108% 108%;-webkit-text-fill-color: white;animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
+.fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o .fitimg.u-pic {border-radius: 100px ;border: 2px solid #ffffff;letter-spacing: 1px;background-size: 108% 108%;-webkit-text-fill-color: white;animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
 	margin-bottom: 15% !important;
 	margin-right: 5% !important;
  }
-.fl.hand.nosel.uzr.uhtml.uid18c2b178444x1f034866x1dzh46 .{
+.fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o .{
     border-radius: 0px 15px 15px 15px;
     display: inline-block;
     background-image: url(https://up6.cc/2023/11/170121230754931.gif);
