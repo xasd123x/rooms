4e7dc5bbd291d5c3d8c427242abb17cfedd3e797
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 00:33:09 +0300

    Update and rename name2.js to name22.js

--- name22.js
+++ name22.js
@@ -13,19 +13,3 @@ $(`<style>
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
 
 
-$(`<style>#mic4 {margin-right: 8px !important;}
-#mic.l7dah1 .mic {
-    border: 0px solid;
-    box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 5px rgb(0 0 0), 0 0 0 0px #000000;
-    margin: 3px;
-    margin-top: 1px;
-	height: 48px;
-}
-@keyframes huerotate {
-    0% {
-        filter: hue-rotate(0deg);
-    }
-    100% {
-        filter: hue-rorate(360deg);
-    }
-}</style>`).insertBefore('body');
