1518f912b7b5d66604fc33499aff7a2db798ef8c
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 01:27:02 +0300

    Update name292.js

--- name292.js
+++ name292.js
@@ -1,17 +1,4 @@
 $(`<style>
-.fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o .fitimg.u-pic {
-  border-radius: 100px;
-  border: 2px solid #cc8321;
-  letter-spacing: 1px;
-  background-size: 108% 108%;
-  -webkit-text-fill-color: white;
-  animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
-  min-width: 62px !important;
-  width: 62px !important;
-  min-height: 60px !important;
-  max-height: 60px !important;
-  display: none;
-}
 .fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o .{
     border-radius: 0px 15px 15px 15px;
     display: inline-block;
