b92653228a174b210ffffdc1ab0ebe0d209d38f9
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 01:39:43 +0300

    Update and rename name242.js to name202.js

---
+++ name202.js
@@ -0,0 +1,11 @@
+$(`<style>
+.fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o {
+    border-radius: 0px 15px 15px 15px;
+    display: inline-block;
+    background-image: url(https://up6.cc/2023/11/170121230754931.gif);
+	background-size: 100%;
+	padding: 5px !important;
+    }
+.fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o .fitimg.u-pic {background-color: transparent;letter-spacing: 1px;-webkit-text-fill-color: #ffffff;padding: 0px !important;border-image: url(https://up6.cc/2023/03/167839668797841.gif);!important;background-color: #transparent;
+}
+@keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
