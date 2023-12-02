1b57bad8aebd27e710903f6732768b2b2faad296
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 01:25:20 +0300

    Update and rename name272.js to name292.js

--- name292.js
+++ name292.js
@@ -1,15 +1,4 @@
 $(`<style>
-.fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o .fitimg.u-pic {border-radius: 100px ;border: 2px solid #ffffff;letter-spacing: 1px;background-size: 108% 108%;-webkit-text-fill-color: white;animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
-	margin-bottom: 15% !important;
-	margin-right: 5% !important;
- }
-.fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o .{
-    border-radius: 0px 15px 15px 15px;
-    display: inline-block;
-    background-image: url(https://up6.cc/2023/11/170121230754931.gif);
-	background-size: 100%;
-	padding: 5px !important;
-    }
 .fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o .fitimg.u-pic {
   border-radius: 100px;
   border: 2px solid #cc8321;
@@ -23,6 +12,14 @@ $(`<style>
   max-height: 60px !important;
   display: none;
 }
+.fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o .{
+    border-radius: 0px 15px 15px 15px;
+    display: inline-block;
+    background-image: url(https://up6.cc/2023/11/170121230754931.gif);
+	background-size: 100%;
+	padding: 5px !important;
+    }
+
 .nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .d-flex.fitimg.u-pic {
   letter-spacing: 1px;
   background-size: 108% 108%;
