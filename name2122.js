3c99430dcef889e1f2d5973a0140a51dc1a14093
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 02:11:27 +0300

    Update and rename name2102.js to name2122.js

--- name2122.js
+++ name2122.js
@@ -1,5 +1,5 @@
 $(`<style>
-.fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o .fitimg.u-pic {border-image: url(https://up6.cc/2023/03/167839668797841.gif)letter-spacing: 1px;background-size: 108% 108%;-webkit-text-fill-color: white;animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
+.fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o .fitimg.u-pic {border-image: url(https://up6.cc/2023/03/167839668797841.gif);letter-spacing: 1px;background-size: 108% 108%;-webkit-text-fill-color: white;animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
 	margin-bottom: 15% !important;
 	margin-right: 5% !important;}
 .fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o {
