dd6257cba1c9057d57d11520d729b79c560ab986
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 00:25:45 +0300

    Update name.js

--- name.js
+++ name.js
@@ -1,1 +1,12 @@
-.fl.hand.nosel.uzr.uhtml.uid18c2b178444x1f034866x1dzh46 lid185baa32203xaac35276x74ifkq" onclick="upro('18c2b178444x1f034866x1dzh46');" uid="18c2b178444x1f034866x1dzh46">
+$(`<style>
+.fl.hand.nosel.uzr.uhtml.uid18c2b178444x1f034866x1dzh46 .fitimg.u-pic {border-radius: 100px ;border: 2px solid #ffffff;letter-spacing: 1px;background-size: 108% 108%;-webkit-text-fill-color: white;animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
+	margin-bottom: 15% !important;
+	margin-right: 5% !important;
+ }
+.fl.hand.nosel.uzr.uhtml.uid18c2b178444x1f034866x1dzh46 .{
+    border-radius: 0px 15px 15px 15px;
+    display: inline-block;
+    background-image: url(https://up6.cc/2023/11/170121230754931.gif);
+	background-size: 100%;
+	padding: 5px !important;
+    }
