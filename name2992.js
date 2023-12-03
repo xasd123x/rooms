7b4694208abe58a058625556fab7849e59bf1ee0
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 08:47:17 +0300

    Update name2992.js

--- name2992.js
+++ name2992.js
@@ -1,10 +1,15 @@
 $(`<style>
-.fl.hand.nosel.uzr.uhtml.uid18c2e14198ex539a1557-x6i7uin .fitimg.u-pic {border-radius: 15px 0px 15px 0px;border: 2px solid #ffffff;);
+.fl.hand.nosel.uzr.uhtml.uid18c2e34a457x7f73cb9-xfrorjx .fitimg.u-pic {border-radius: 15px 0px 15px 0px;border: 2px solid #ffffff;);
 }
-.fl.hand.nosel.uzr.uhtml.uid18c2e14198ex539a1557-x6i7uin {
+.fl.hand.nosel.uzr.uhtml.uid18c2e34a457x7f73cb9-xfrorjx {
     display: inline-block;
     border-radius: 15px 0px 15px 0px;border: 2px solid #ffffff;
     background-image: url(https://up6.cc/2023/11/170121230754931.gif);
 	background-size: 100%;
     }
+.fl.hand.nosel.uzr.uhtml.uid18c2e34a457x7f73cb9-xfrorjx .mini.u-msg {
+    text-align: center;
+    -webkit-text-fill-color: #ffffff;
+    background-image: linear-gradient(-225deg, #c8c8c8 0%, #ffffff 29%, #c8c8c8 67%, #ffffff 100%);background-size: 220% auto;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 1s linear infinite;display: inline-block;
+    }    
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
