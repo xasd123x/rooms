2e4b4f2ce8fedbf52e5e2de4baf371dfe48dc4d1
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 09:47:18 +0300

    Update and rename name313.js to name312.js

--- name312.js
+++ name312.js
@@ -12,4 +12,11 @@ $(`<style>
     -webkit-text-fill-color: #ffffff;
     background-image: url(https://up6.cc/2023/02/167536001950611.gif);background-size: 220% auto;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 1s linear infinite;display: inline-block;
     }
+
+.fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .uzr.flex-fill break {
+    display: inline-block;
+    border-radius: 15px 0px 15px 0px;border: 2px solid #424242;
+    background-image: url(https://up6.cc/2023/12/170158549655641.jpg);
+	background-size: 100%;
+    }
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
