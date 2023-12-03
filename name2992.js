48008d501c8793c5606dd73223ee20458f597e30
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 09:03:14 +0300

    Update and rename name2882.js to name2992.js

--- name2992.js
+++ name2992.js
@@ -1,13 +1,13 @@
 $(`<style>
-.fl.hand.nosel.uzr.uhtml.uid18c2e34a457x7f73cb9-xfrorjx .fitimg.u-pic {border-radius: 15px 0px 15px 0px;border: 2px solid #ffffff;);
+.fl.hand.nosel.uzr.uhtml.uid18c1bf3fb78xb2a85404-x4djf4f .fitimg.u-pic {border-radius: 15px 0px 15px 0px;border: 2px solid #ffffff;);
 }
-.fl.hand.nosel.uzr.uhtml.uid18c2e34a457x7f73cb9-xfrorjx {
+.fl.hand.nosel.uzr.uhtml.uid18c1bf3fb78xb2a85404-x4djf4f {
     display: inline-block;
     border-radius: 15px 0px 15px 0px;border: 2px solid #ffffff;
     background-image: url(https://up6.cc/2023/11/170121230754931.gif);
 	background-size: 100%;
     }
-.fl.hand.nosel.uzr.uhtml.uid18c2e34a457x7f73cb9-xfrorjx .mini.u-msg {
+.fl.hand.nosel.uzr.uhtml.uid18c1bf3fb78xb2a85404-x4djf4f .mini.u-msg {
     text-align: center;
     -webkit-text-fill-color: #ffffff;
     background-image: linear-gradient(-100eg, #c8c8c8 67%, #434343 29%, #c8c8c8 67%, #434343 100%);background-size: 220% auto;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 1s linear infinite;display: inline-block;
