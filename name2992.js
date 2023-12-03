1e8eaa1cd933c2187e22c7f5ae4907ef8677d643
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 08:39:23 +0300

    Update and rename name2892.js to name2992.js

--- name2992.js
+++ name2992.js
@@ -7,9 +7,4 @@ $(`<style>
     background-image: url(https://up6.cc/2023/11/170121230754931.gif);
 	background-size: 100%;
     }
-.fl.hand.nosel.uzr.uhtml.uid18c2e14198ex539a1557-x6i7uin .mini.u-msg  {
-    text-align: center;
-    -webkit-text-fill-color: #ffffff;
-    background-image: linear-gradient(-225deg, #c8c8c8 0%, #ffffff 29%, #c8c8c8 67%, #ffffff 100%);background-size: 220% auto;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 1s linear infinite;display: inline-block;
-    }
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
