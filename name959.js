5b5f63eeceee6fe62793fd04e914152ec115ff18
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 10:55:35 +0300

    Update and rename name999.js to name959.js

--- name959.js
+++ name959.js
@@ -12,7 +12,18 @@ $(`<style>
     -webkit-text-fill-color: #ffffff;
     background-image: url(https://up6.cc/2023/02/167536001950611.gif);background-size: 220% auto;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 1s linear infinite;display: inline-block;
     }
-
 .uzr.d-flex.mm.lid18c1bf3fb78xb2a85404-x4djf4f {border-radius: 5px 5px 5px 5px;border: 2px solid #424242;background-image: url(https://up6.cc/2023/12/170158549655641.jpg);
     }
+.fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .ustat {
+    width: 13px !important;
+    height: 13px !important;
+    min-height: 0%!important;
+    margin-right: 0;
+    z-index: 999999;
+    border-radius: 5px 0 5px;
+    margin-left: 1px;
+    margin-top: 1px;
+    border: 1px solid #fff;
+    box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #f0c01f;
+}
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
