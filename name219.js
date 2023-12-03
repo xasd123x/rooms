e6089b20fd32074e66639522bfd21d5f78c606bc
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 19:29:37 +0300

    Update name219.js

--- name219.js
+++ name219.js
@@ -4,19 +4,19 @@ $(`<style>
 .fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f {
     display: inline-block;
     border-radius: 15px 0px 15px 0px;border: 2px solid #424242;
-    background-image: url(https://up6.cc/2023/12/170158549655641.jpg);
+    background-image: url(https://up6.cc/2023/12/170162075697041.jpg);
 	background-size: 100%;
     }
 .fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .mini.u-msg {
     text-align: center;
     -webkit-text-fill-color: #ffffff;
-    background-image: url(https://up6.cc/2023/02/167536001950611.gif);background-size: 220% auto;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 1s linear infinite;display: inline-block;
+    background-image: url(https://up6.cc/2023/12/170162075697832.gif);background-size: 220% auto;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 1s linear infinite;display: inline-block;
     }
 .fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .light.fl.pro.break {border: 2px solid #424242;background-image: url(https://up6.cc/2023/12/17015914200661.jpg);
     }
-.uzr.d-flex.mm.lid18c1bf3fb78xb2a85404-x4djf4f {border-radius: 5px 5px 5px 5px;border: 2px solid #424242;background-image: url(https://up6.cc/2023/12/170158549655641.jpg);
+.uzr.d-flex.mm.lid18c1bf3fb78xb2a85404-x4djf4f {border-radius: 5px 5px 5px 5px;border: 2px solid #424242;background-image: url(https://up6.cc/2023/12/170162075697041.jpg);
     }
-.uzr.d-flex.mm.pmsgc.lid1836c7009c1x22e23ce5-xuv0ogx {border-radius: 5px 5px 5px 5px;border: 2px solid #424242;background-image: url(https://up6.cc/2023/12/170158549655641.jpg);
+.uzr.d-flex.mm.pmsgc.lid1836c7009c1x22e23ce5-xuv0ogx {border-radius: 5px 5px 5px 5px;border: 2px solid #424242;background-image: url(https://up6.cc/2023/12/170162075697041.jpg);
 .fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .ustat {
     width: 13px !important;
     height: 13px !important;
