af216f01416cf34803e3be3b1b532c7f2f757c4a
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 08:17:33 +0300

    Update name2182.js

--- name2182.js
+++ name2182.js
@@ -1,7 +1,7 @@
 $(`<style>
-.fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o .fitimg.u-pic {border-radius: 15px 0px 15px 0px;border: 2px solid #ffffff;);
+.fl.hand.nosel.uzr.uhtml.uid18c2e14198ex539a1557-x6i7uin .fitimg.u-pic {border-radius: 15px 0px 15px 0px;border: 2px solid #ffffff;);
 }
-.fl.hand.nosel.uzr.uhtml.uid18c2bf8e193x0e1e5025x29gg9o {
+.fl.hand.nosel.uzr.uhtml.uid18c2e14198ex539a1557-x6i7uin {
     display: inline-block;
     border-radius: 15px 0px 15px 0px;border: 2px solid #ffffff;
     background-image: url(https://up6.cc/2023/11/170121230754931.gif);
