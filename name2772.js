536df780beeef6ccc87e609e37e1715a5f20b763
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 08:57:20 +0300

    Update and rename name2662.js to name2772.js

--- name2772.js
+++ name2772.js
@@ -10,6 +10,6 @@ $(`<style>
 .fl.hand.nosel.uzr.uhtml.uid18c2e34a457x7f73cb9-xfrorjx .mini.u-msg {
     text-align: center;
     -webkit-text-fill-color: #ffffff;
-    background-image: linear-gradient(-225deg, #c8c8c8 0%, #434343 29%, #c8c8c8 67%, #ffffff 100%);background-size: 220% auto;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 1s linear infinite;display: inline-block;
+    background-image: linear-gradient(-225deg, #c8c8c8 67%, #434343 29%, #c8c8c8 67%, #434343 100%);background-size: 220% auto;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 1s linear infinite;display: inline-block;
     }    
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
