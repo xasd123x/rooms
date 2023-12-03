083ef3a6ebde04497ea49e93560e042831c3d2d2
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-03 09:07:39 +0300

    Update and rename name2332.js to name23432.js

--- name23432.js
+++ name23432.js
@@ -10,6 +10,6 @@ $(`<style>
 .fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .mini.u-msg {
     text-align: center;
     -webkit-text-fill-color: #ffffff;
-    background-image: linear-gradient(-100eg, #c8c8c8 67%, #434343 29%, #c8c8c8 67%, #434343 100%);background-size: 220% auto;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 1s linear infinite;display: inline-block;
+    background-image: linear-gradient(-225eg, #c8c8c8 67%, #434343 29%, #c8c8c8 67%, #434343 100%);background-size: 220% auto;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 1s linear infinite;display: inline-block;
     }    
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
