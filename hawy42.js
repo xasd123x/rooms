f5fc12979f93d93f713c7d9efd96169b256fa0eb
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-04 17:10:58 +0300

    Update and rename hawy41.js to hawy42.js

--- hawy42.js
+++ hawy42.js
@@ -10,10 +10,10 @@ $(`<style>
 .fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f {
     background-size: 100%;
     display: inline-block;
-    min-height: 0%!important;    
-    margin-right: 0;
+    min-height: 5%!important;    
+    margin-right: 2;
     margin-left: 2px;
-    margin-top: 3px;
+    margin-top: 2px;
     border-radius: 15px 0px 15px 0px;border: 1px solid #424242;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;
     background-image: url(https://up6.cc/2023/12/170162622444081.jpg);
     }
