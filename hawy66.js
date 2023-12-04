a557c55b722131eba543aa231bca3cb13e920a84
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-04 18:59:13 +0300

    Update and rename hawy65.js to hawy66.js

--- hawy66.js
+++ hawy66.js
@@ -38,8 +38,8 @@ $(`<style>
     margin-right: 0;
     z-index: 999999;
     border-radius: 5px 0 5px;
-    margin-left: 4px;
-    margin-top: 1px;
+    margin-left: 2px;
+    margin-top: 5px;
     border: 1px solid #fff;
     box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #f0c01f;
 }
