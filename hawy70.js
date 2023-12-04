1e9e54b2c59188958972ebcf0030e3647b7f3f36
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-04 19:32:38 +0300

    Update and rename hawy69.js to hawy70.js

--- hawy70.js
+++ hawy70.js
@@ -1,5 +1,5 @@
 $(`<style>
-.fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .fitimg.u-pic {background-size: 100%;border-radius: 15px 0px 15px 0px;border: 1px solid #ffffff;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;
+.fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .fitimg.u-pic {background-size: 100%;padding: 0px !important;border-radius: 15px 0px 15px 0px;border: 1px solid #ffffff;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;
 }
 .fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .u-topic {    
     margin-right: 2px!important; 
