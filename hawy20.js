565ce5a56079311eb6df1cce470beb10ee097b3b
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-04 23:18:11 +0300

    Update and rename hawy19.js to hawy20.js

--- hawy20.js
+++ hawy20.js
@@ -22,8 +22,9 @@ $(`<style>
 .uzr.d-flex.mm.lid18c1bf3fb78xb2a85404-x4djf4f {border-radius: 5px 5px 5px 5px;
     margin-top: 2px!important;
     margin-bottom: 2px!important;
-    border-bottom: 0.5px solid #000000!important;
-    border: 0.5px solid #000000;
+    border-bottom: 0.5px solid #dddddd!important;
+    border: 0.5px solid #dddddd;
+    box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #acacac;
     }
 .uzr.d-flex.mm.lid18c1bf3fb78xb2a85404-x4djf4f .fitimg.u-pic {border-radius: 15px 0px 15px 0px;border: 1px solid #ffffff;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;background-size: 100%;
 }
