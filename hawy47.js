fbeb0b363b6b45de05356f06c21e0b0e7fe9db68
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-04 17:44:40 +0300

    Update and rename hawy46.js to hawy47.js

--- hawy47.js
+++ hawy47.js
@@ -11,8 +11,8 @@ $(`<style>
     background-size: 100%;
     display: inline-block;
     min-height: 0%!important;    
-    margin-right: 2px!important;
-    margin-left: 6px!important;
+    margin-right: 6px!important;
+    margin-left: 2px!important;
     margin-top: 3px!important;
     border-radius: 15px 0px 15px 0px;border: 1px solid #424242;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;
     background-image: url(https://up6.cc/2023/12/170162622444081.jpg);
