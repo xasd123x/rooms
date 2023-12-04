10854af8bb0c9575f5101476e7ced183cbb51820
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-04 18:48:52 +0300

    Update and rename hawy61.js to hawy62.js

--- hawy62.js
+++ hawy62.js
@@ -14,14 +14,14 @@ $(`<style>
     margin-right: 3px!important;
     margin-left: 2px!important;
     margin-top: 2px!important;
-    margin-bottom: 13px!important;
+    margin-bottom: 2px!important;
     }
 .fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .mini.u-msg {
     text-align: center;
     background-image: url(https://up6.cc/2023/12/170162505647371.gif);margin-right: 3px!important;
     margin-left: 2px!important;
     margin-top: 2px!important;
-    margin-down: 20px!important;-webkit-background-clip: text;-webkit-text-fill-color: transparent;display: inline-block;background-size: 220% auto;
+    margin-bottom: 20px!important;-webkit-background-clip: text;-webkit-text-fill-color: transparent;display: inline-block;background-size: 220% auto;
     }
 .fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .light.fl.pro.break {border: 2px solid #424242;background-image: url(https://up6.cc/2023/12/17015914200661.jpg);
     }
