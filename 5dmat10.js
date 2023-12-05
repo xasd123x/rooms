155d3471e9a7109645098286fc00513f002d77a4
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-05 19:28:41 +0300

    Update and rename 5dmat9.js to 5dmat10.js

--- 5dmat10.js
+++ 5dmat10.js
@@ -3,7 +3,7 @@ $("style:eq(0)").append(`#d2 .roomh[onclick="rjoin('${rid}')"]{
 background-size: 220% auto;border-radius: 50px !important;border: 1px solid #0000;
 }`);
 $("style:eq(0)").append(`#upro .roomh[onclick="rjoin('${rid}')"]{
-background-image: url(https://up6.cc/2023/12/170161252999561.png); background-size: cover;border-radius: 50px !important;color: #ffffff;
+background-image: url(https://up6.cc/2023/12/170161252999561.png); background-size: cover;border-radius: 20px !important;color: #ffffff;
 }`);
 
 $(`<style>
