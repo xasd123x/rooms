1ac81298c0326e3ef45415129d15636551cef3bc
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-06 23:44:12 +0300

    Update and rename adarh2.js to adarh3.js

--- adarh3.js
+++ adarh3.js
@@ -319,7 +319,7 @@ var room-adarh = false;
 setInterval(function () {
     if (myroom == '1872f700a8ex138ba961x-m19n3h') {
         if (room-adarh == false) {
-            room-adarhroom-adarh = true;
+            room-adarh = true;
                 $('#d0, #users, #d2, #mic, #settings, #rooms, #room, #d0, #chats, #d2bc, #muteall, #wall, #dpnl, #upro').addClass('room-adarh1');
 		$('input, button.rsave.btn.btn-primary.fl, button.border.btn.label.label-success.fl.fa.fa-plus, button.blike.corner.btn.minix.btn-danger.fa.fa-heart, .fl.borderg, .fl.u-pic.fitimg, .dots, .tc, .label.fl.label-primary, .btn.btn-primary.u-nickc.fr.fa.fa-save, .modal-header.label-primary, .corner.border.label.label-primary,.head.d-flex.nosel.bg.fl, .rsave.btn.btn-primary.fr, .fa.fa-send.sndpm.fl.btn.btn-primary, .fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary, .fr.fa.fa-share-alt.sndfilebc.fl.btn.btn-primary, .bdel.corner.btn.minix.btn-primary.fa.fa-times, .fa.fa-send.sndbc.fl.btn.btn-primary').addClass('room-adarh1');
         }
