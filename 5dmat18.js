cccdf66407ff6d51eb61fbaa11a98e5959438d64
flhchat <151204521+flhchat@users.noreply.github.com>
2023-12-06 19:54:49 +0300

    Update and rename 5dmat17.js to 5dmat18.js

---
+++ 5dmat18.js
@@ -0,0 +1,9 @@
+var rid='18c1d51b916x6921ecex-7yj6pt';
+$("style:eq(0)").append(`#img .My_itar[onclick="rjoin('${rid}')"]{margin-left: 5px;margin-right: auto;min-width: 20px !important;width: 20px !important;min-height: 50px !important;max-height: 50px !important;margin-top: 20px;" src(https://i.ibb.co/HdQKHZK/image.gif);
+}`);
+$("style:eq(0)").append(`#d2 .roomh[onclick="rjoin('${rid}')"]{
+background-size: 220% auto;border-radius:  50px !important;border: 1px solid #0000;
+}`);
+$("style:eq(0)").append(`#upro .roomh[onclick="rjoin('${rid}')"]{
+background-image: url(https://up6.cc/2023/11/170009087923123.png); background-size: cover;color: #ffffff;
+}`);
