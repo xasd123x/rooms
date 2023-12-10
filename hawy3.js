105cb8e9982850561e54752f591eec83647d06aa
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-10 22:47:00 +0300

    Update hawy3.js

--- hawy3.js
+++ hawy3.js
@@ -1,4 +1,4 @@
-var lid="17f50b8ef6fx1a21c9a4-x2s7ldm"';
+var lid='17f50b8ef6fx1a21c9a4-x2s7ldm';
 $("style:eq(0)").append(`#upro .17f50b8ef6fx1a21c9a4-x2s7ldm [onclick="rjoin('${lid}')"]{
     background-image: url(https://up6.cc/2023/12/170215305562121.jpg);
     background-size: 100%;
