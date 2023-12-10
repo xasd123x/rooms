3e6f32c805cecc060669183c498d49954568e807
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-10 23:19:47 +0300

    Update and rename hawy8.js to hawy9.js

--- hawy9.js
+++ hawy9.js
@@ -41,8 +41,7 @@ margin-bottom: 2px!important;
     box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #424242;
 }
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
-var rid='lid17f50b8ef6fx1a21c9a4-x2s7ldm';
 $("style").last().append(`#upro .light.fl.pro.break {
-    background-image: url(https://up6.cc/2023/12/170215305562121.jpg);
+    background-image: url(https://up6.cc/2023/12/17015914200661.jpg);
     background-size: 100%;
 }`);
