b3ee182940337caafd47dc535ef3b3fa7bf25484
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-11 23:20:28 +0300

    Update and rename hawy-t1.js to hawy-t2.js

--- hawy-t2.js
+++ hawy-t2.js
@@ -14,7 +14,7 @@ $("style").last().append(`
 var l_itvhawy=setInterval(function(){
     if(myid!=null){
         clearInterval(l_itvhawy);
-        $("#users .uzr:contains('00.0')").addClass('hawy1');
+        $("#users .uzr:contains('.00.0..')").addClass('hawy1');
         $("#ifr_mov1").show(function(){
         $("#upro").modal("hide");
     } ); 
