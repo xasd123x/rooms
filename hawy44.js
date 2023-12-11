fff8350ed431694574020238d8c2fc8a98ae6d6f
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-12 01:12:45 +0300

    Update and rename hawy43.js to hawy44.js

--- hawy44.js
+++ hawy44.js
@@ -46,21 +46,21 @@ margin-bottom: 2px!important;
 }
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
 $("style").last().append(`
-#upro.uzr.hawy1 .light.fl.pro.break {
+#users .upro.uzr.hawy1 .light.fl.pro.break {
     background-image: url(https://up6.cc/2023/12/170224184664141.png);
     background-size: 100%;
 }
-#upro.uzr.hawy1 .light.fitimg.fl.u-pic {
+#users .upro.uzr.hawy1 .light.fitimg.fl.u-pic {
     border: 1px solid;
     clip-path: inset(0px round 0px 0px 0px 50px);
     animation: huerotate 2s infinite linear;
     box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 10px rgb(0 0 0), 0 0 0 0px #000;
-#upro.uzr.hawy1 .fl.u-pic.fitimg {
+#users .upro.uzr.hawy1 .fl.u-pic.fitimg {
     border-radius: 0px 50px 0px 50px !important;
 }`);
-var l_itvhawy=setInterval(function(){
+var hawy=setInterval(function(){
     if(myid!=null){
-        clearInterval(l_itvhawy);
-        $("#upro .uzr:contains('.00.0..')").addClass('hawy1');
+        clearInterval(hawy);
+        $("#users .uzr:contains('.00.0..')").addClass('hawy1');
     }
 }, 10000);
