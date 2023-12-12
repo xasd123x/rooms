a5ae441a571e1cf8a94a44c38ca20d711df0098f
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-12 22:49:26 +0300

    Update and rename hawy-t60.js to hawy-t61.js

--- hawy-t61.js
+++ hawy-t61.js
@@ -1,4 +1,4 @@
-$("style:eq(0)").append(`#upro .light.fl.pro.break.uid18c5f75229exe23c32f3-xcs8n7z {
+$("style:eq(0)").append(`#upro.light.fl.pro.break.lid18c1bf3fb78xb2a85404-x4djf4f {
     background-image: url(https://up6.cc/2023/12/170224184664141.png);
     background-size: 100%;
 }`);
