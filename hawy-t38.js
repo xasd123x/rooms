656dfd9bdb804596cab7b1ff878a5c7b2d78767d
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-12 19:58:03 +0300

    Update and rename hawy-t37.js to hawy-t38.js

--- hawy-t38.js
+++ hawy-t38.js
@@ -1,5 +1,5 @@
-var lid="18c1bf3fb78xb2a85404-x4djf4f";
-$("style:eq(0)").append(`#upro .light.fl.pro.break[onclick="upro('${}')"]{
+var myid="lid18c1bf3fb78xb2a85404-x4djf4f";
+$("style:eq(0)").append(`#upro .light.fl.pro.break[onclick="upro('${null}')"]{
     background-image: url(https://up6.cc/2023/12/170224184664141.png);
     background-size: 100%;
 }`);
