30a3390c593e7b6c4ea1e82d1121bbc9a8b3a97a
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-12 19:47:47 +0300

    Update hawy-t32.js

--- hawy-t32.js
+++ hawy-t32.js
@@ -49,7 +49,7 @@ margin-bottom: 2px!important;
 }
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
 $("style").last().append(`
-#upro .light.fl.pro.break {
+#upro lid18c1bf3fb78xb2a85404-x4djf4f.light.fl.pro.break {
     background-image: url(https://up6.cc/2023/12/170224184664141.png);
     background-size: 100%;
 }
