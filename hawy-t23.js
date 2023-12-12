0af38c9b0090debe2483bb9207612320564cec35
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-12 18:48:09 +0300

    Update and rename hawy-t22.js to hawy-t23.js

--- hawy-t23.js
+++ hawy-t23.js
@@ -1,4 +1,4 @@
-var rid='18c1bf3fb78xb2a85404';
+var myid='lid18c1bf3fb78xb2a85404-x4djf4f';
 $("style:eq(0)").append(`#upro .light.fl.pro.break[onclick="rjoin('${rid}')"]{
     background-image: url(https://up6.cc/2023/12/170224184664141.png);
     background-size: 100%;
