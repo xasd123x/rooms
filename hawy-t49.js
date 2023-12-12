c8f4405f2d9fb6236f11d3a822d0ed7cefd1b928
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-12 22:25:37 +0300

    Update and rename hawy-t48.js to hawy-t49.js

--- hawy-t49.js
+++ hawy-t49.js
@@ -44,7 +44,7 @@ margin-bottom: 2px!important;
 }
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
 $("style").last().append(`
-#upro .hawy1.light.fl.pro.break {
+#upro.modal.in .hawy1.light.fl.pro.break {
     background-image: url(https://up6.cc/2023/12/170224184664141.png);
     background-size: 100%;
 }
