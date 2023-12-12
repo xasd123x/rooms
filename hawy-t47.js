d3b76cefc30b505457c33f11afa6c103e5ff924d
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-12 20:54:25 +0300

    Update and rename hawy-t46.js to hawy-t47.js

--- hawy-t47.js
+++ hawy-t47.js
@@ -44,16 +44,16 @@ margin-bottom: 2px!important;
 }
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
 $("style").last().append(`
-#upro.hawy1 .light.fl.pro.break {
+#upro .hawy1.light.fl.pro.break {
     background-image: url(https://up6.cc/2023/12/170224184664141.png);
     background-size: 100%;
 }
-#upro.hawy1 .light.fitimg.fl.u-pic {
+#upro .hawy1.light.fitimg.fl.u-pic {
     border: 1px solid;
     clip-path: inset(0px round 0px 0px 0px 50px);
     animation: huerotate 2s infinite linear;
     box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 10px rgb(0 0 0), 0 0 0 0px #000;
-#upro.hawy1 .fl.u-pic.fitimg {
+#upro .hawy1.fl.u-pic.fitimg {
     border-radius: 0px 50px 0px 50px !important;
 }`);
 var l_itvhawy=setInterval(function(){
