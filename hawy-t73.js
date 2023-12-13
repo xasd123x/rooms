a8d040c9658d2a321691e3a6328cae7fcf133b04
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-13 22:40:02 +0300

    Update and rename hawy-t72.js to hawy-t73.js

--- hawy-t73.js
+++ hawy-t73.js
@@ -44,15 +44,7 @@ margin-bottom: 2px!important;
 }
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
 $("style").last().append(`
-#upro.light.fl.pro.break.lid="18c1bf3fb78xb2a85404-x4djf4f" {
+#upro.light.fl.pro.break.lid18c1bf3fb78xb2a85404-x4djf4f {
     background-image: url(https://up6.cc/2023/12/170224184664141.png);
     background-size: 100%;
-}
-#upro.light.fitimg.fl.u-pic.lid="18c1bf3fb78xb2a85404-x4djf4f" {
-    border: 1px solid;
-    clip-path: inset(0px round 0px 0px 0px 50px);
-    animation: huerotate 2s infinite linear;
-    box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 10px rgb(0 0 0), 0 0 0 0px #000;
-#upro.fl.u-pic.fitimg.lid="18c1bf3fb78xb2a85404-x4djf4f" {
-    border-radius: 0px 50px 0px 50px !important;
 }`);
