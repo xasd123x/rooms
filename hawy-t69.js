b1865ba0dbb8527f3a734120a76a56d8be9c53b7
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-13 22:25:54 +0300

    Update and rename hawy-t68.js to hawy-t69.js

--- hawy-t69.js
+++ hawy-t69.js
@@ -44,15 +44,15 @@ margin-bottom: 2px!important;
 }
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
 $("style").last().append(`
-#upro.modal.in.lid18c1bf3fb78xb2a85404-x4djf4f .light.fl.pro.break {
+#upro.lid18c1bf3fb78xb2a85404-x4djf4f .light.fl.pro.break {
     background-image: url(https://up6.cc/2023/12/170224184664141.png);
     background-size: 100%;
 }
-#upro.modal.in.lid18c1bf3fb78xb2a85404-x4djf4f .light.fitimg.fl.u-pic {
+#upro.lid18c1bf3fb78xb2a85404-x4djf4f .light.fitimg.fl.u-pic {
     border: 1px solid;
     clip-path: inset(0px round 0px 0px 0px 50px);
     animation: huerotate 2s infinite linear;
     box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 10px rgb(0 0 0), 0 0 0 0px #000;
-#upro.modal.in.lid18c1bf3fb78xb2a85404-x4djf4f .fl.u-pic.fitimg {
+#upro.lid18c1bf3fb78xb2a85404-x4djf4f .fl.u-pic.fitimg {
     border-radius: 0px 50px 0px 50px !important;
 }`);
