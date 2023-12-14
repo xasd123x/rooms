a8333af7a205256572000cde2cb14c7dab052aef
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-14 20:26:37 +0300

    Update and rename hawy-tt11.js to hawy-tt12.js

--- hawy-tt12.js
+++ hawy-tt12.js
@@ -41,7 +41,7 @@ margin-bottom: 2px!important;
     box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #424242;
 }
 .nosel.fl.bg {
-    background-image: url(https://up6.cc/2023/12/170257320247871.png);
+    background-image: url(https://up6.cc/2023/12/170257478081611.gif);
     background-size: 100%;
 }
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
