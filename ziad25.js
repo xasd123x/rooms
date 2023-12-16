b3c6e62fed17ad3050ec44b88044f2f253722222
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-16 21:01:41 +0300

    Update and rename ziad24.js to ziad25.js

--- ziad25.js
+++ ziad25.js
@@ -20,7 +20,7 @@ $(`<style>
     background-image: url(https://up6.cc/2023/12/170274954735011.gif);margin-right: 3px!important;
     margin-left: 2px!important;
     margin-top: 10px!important;
-    margin-bottom: 18px!important;-webkit-background-clip: text;-webkit-text-fill-color: transparent;display: inline-block;background-size: 170% auto;
+    margin-bottom: 18px!important;-webkit-background-clip: text;-webkit-text-fill-color: transparent;display: inline-block;background-size: 100% auto;
     }
 .uzr.d-flex.mm.lid18c1bf3fb78xb2a85404-x4djf4f {
     border-radius: 5px 5px 5px 5px;
