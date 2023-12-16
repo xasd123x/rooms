ddc63389e70d76c227d66ce740d0c04bd5d3a2e9
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-16 19:55:19 +0300

    Update and rename ziad2.js to ziad3.js

--- ziad3.js
+++ ziad3.js
@@ -1,7 +1,10 @@
 $(`<style>
-.fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .fitimg.u-pic {width: 58px !important;
-    height: 58px !important;border-radius: 15px 0px 15px 0px !important;
-    border: 1px solid #ffffff;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;background-size: 100%;margin-top: 5px!important;margin-bottom: 4px!important;
+.fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .fitimg.u-pic {
+  border-radius: 100px !important;
+  margin-top: 10px !important;
+  height: 55px !important;
+  margin-left: -5px !important;
+  width: 55px;
 }
 .fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f {
     background-size: 100% auto;
