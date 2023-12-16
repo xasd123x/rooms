a3133837431fc5629a8c15454736deb85b07b3be
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-16 20:14:33 +0300

    Update and rename ziad16.js to ziad17.js

--- ziad17.js
+++ ziad17.js
@@ -49,7 +49,7 @@ margin-bottom: 2px!important;
 $("style").last().append(`
 #users .uzr.ziad1 .ziad_i6ar1 {
 width: 92px;
-margin-top: -20px;
+margin-top: -25px;
 height: 92px;
 margin-left: -17px;
 background-size: 100%;
