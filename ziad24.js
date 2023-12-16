18c716820c7e9ba8f99740789a681c4ddc41c02b
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-16 21:00:13 +0300

    Update and rename ziad23.js to ziad24.js

--- ziad24.js
+++ ziad24.js
@@ -7,7 +7,7 @@ $(`<style>
   width: 55px;
 }
 .fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f {
-    background-size: 100% auto;
+    background-size: 100% ;
     border-radius: 15px 0px 15px 0px;border: 1px solid #424242;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;
     background-image: url(https://up6.cc/2023/12/170274434099671.png);
     margin-right: 0px!important;
