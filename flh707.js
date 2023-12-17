70bf4cd47d7dee04db25831b4d6b2f9ce7e8bd34
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-17 22:18:30 +0300

    Update flh707.js

--- flh707.js
+++ flh707.js
@@ -1,42 +1,48 @@
+var rid='18c2afa7cadxc9c5e09x-qc0q8j';
+$("style:eq(0)").append(`#d2 .roomh[onclick="rjoin('${rid}')"]{
+background-size: 220% auto;border-radius: 50px !important;border: 1px solid #0000;
+}`);
+$("style:eq(0)").append(`#upro .roomh[onclick="rjoin('${rid}')"]{
+background-image: url(https://up6.cc/2023/12/170161252999561.png); background-size: cover;border-radius: 50px !important;color: #ffffff;
+}`);
 
 $(`<style>
-.nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .d-flex.fitimg.u-pic {
+.nosel.d-flex.room.r18c2afa7cadxc9c5e09x-qc0q8j .d-flex.fitimg.u-pic {
   letter-spacing: 1px;
   background-size: 108% 108%;
   -webkit-text-fill-color: white;
   animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;
 }
-.nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .label.label-primary.fa.fa-user.uc {
+.nosel.d-flex.room.r18c2afa7cadxc9c5e09x-qc0q8j .label.label-primary.fa.fa-user.uc {
   letter-spacing: 1px;
   -webkit-text-fill-color: #fff;
+  background-color: #170b0b !important;
   border-radius: 15px 0px 15px 0px !important;
   height: 27px;
   margin-top: 40px !important;
   margin-bottom: 0px !important;
-  background-color: #d9534f00;
-  text-align: left!important;
   margin-left: 45px!important;
 }
-.nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r.bord {border-inline: 0px solid #4c121e !important;}
-.nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .mini.u-msg.flex-grow-1 {
+.nosel.d-flex.room.r18c2afa7cadxc9c5e09x-qc0q8j.bord {border-inline: 0px solid #4c121e !important;}
+.nosel.d-flex.room.r18c2afa7cadxc9c5e09x-qc0q8j .mini.u-msg.flex-grow-1 {
     text-align: center;
     margin-top: 53px !important;
 	margin-left: 11px;
 	-webkit-text-fill-color: transparent;
 	animation: textclip 2s linear infinite;
-	background-image: url(https://up6.cc/2023/02/167536001950611.gif);
+	background-image: url(https://up6.cc/2023/12/170146864300182.gif);
 	-webkit-background-clip: text;
 	color: transparent !important;
 	background-size: 90% 100%;
 	opacity:0;
 }
-.nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r {
-    background-image: url(https://up6.cc/2023/11/170121230754931.gif);
+.nosel.d-flex.room.r18c2afa7cadxc9c5e09x-qc0q8j {
+    background-image: url(https://up6.cc/2023/12/170147034248551.gif);
     background-size: cover;
-    border-radius: 15px 0px 15px 0px;
+    border-radius: 15px 0px 15px 0px;border: 2px solid #424242;
     display: inline-block;
 }
-.nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .d-flex.fitimg.u-pic {
+.nosel.d-flex.room.r18c2afa7cadxc9c5e09x-qc0q8j .d-flex.fitimg.u-pic {
   border-radius: 100px;
   border: 2px solid #cc8321;
   letter-spacing: 1px;
@@ -49,7 +55,7 @@ $(`<style>
   max-height: 60px !important;
   display: none;
 }
-.nosel.d-flex.room.r18c1c14a6c4x52a495a1-xl7kq5r .u-topic.dots.flex-grow-1 {
+.nosel.d-flex.room.r18c2afa7cadxc9c5e09x-qc0q8j .u-topic.dots.flex-grow-1 {
     text-transform: uppercase;
     -webkit-text-fill-color: transparent;
     animation: textclip 2s linear infinite;
@@ -64,29 +70,3 @@ $(`<style>
     margin-left: 5px;}
 
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
-
-
-$(`<style>#mic4 {margin-right: 8px !important;}
-#mic.l7dah1 .mic {
-    border: 0px solid;
-    box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 5px rgb(0 0 0), 0 0 0 0px #000000;
-    margin: 3px;
-    margin-top: 1px;
-	height: 48px;
-}
-@keyframes huerotate {
-    0% {
-        filter: hue-rotate(0deg);
-    }
-    100% {
-        filter: hue-rorate(360deg);
-    }
-}</style>`).insertBefore('body');
-
-var rid='18c1c14a6c4x52a495a1-xl7kq5r';
-$("style:eq(0)").append(`#d2 .roomh[onclick="rjoin('${rid}')"]{
-background-size: 220% auto;border-radius:  50px !important;border: 1px solid #0000;
-}`);
-$("style:eq(0)").append(`#upro .roomh[onclick="rjoin('${rid}')"]{
-background-image: url(https://up6.cc/2023/11/170009087923123.png); background-size: cover;color: #ffffff;
-}`);
