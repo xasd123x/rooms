b360e74fe0ce5c8b0c8c9e0d71717a234a69806a
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-17 22:24:31 +0300

    Update and rename flh708.js to flh709.js

--- flh709.js
+++ flh709.js
@@ -1,8 +1,7 @@
-var rid='18c2afa7cadxc9c5e09x-qc0q8j';
-$("style:eq(0)").append(`#d2 .roomh[onclick="rjoin('${rid}')"]{
+$("style:eq(0)").append(`#d2 .roomh[onclick="rjoin('18c2afa7cadxc9c5e09x-qc0q8j')"]{
 background-size: 220% auto;border-radius: 50px !important;border: 1px solid #0000;
 }`);
-$("style:eq(0)").append(`#upro .roomh[onclick="rjoin('${rid}')"]{
+$("style:eq(0)").append(`#upro .roomh[onclick="rjoin('18c2afa7cadxc9c5e09x-qc0q8j')"]{
 background-image: url(https://up6.cc/2023/12/170161252999561.png); background-size: cover;border-radius: 50px !important;color: #ffffff;
 }`);
 $(`<style>
