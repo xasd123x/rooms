060599e06c1d33fb6c71c7a2e7ae52ed36170680
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-17 22:00:35 +0300

    Update and rename hawy-tt66.js to hawy-tt67.js

--- hawy-tt67.js
+++ hawy-tt67.js
@@ -1,5 +1,5 @@
 var lid="18c1bf3fb78xb2a85404-x4djf4f";
-$("style:eq(0)").append(`#upro .pro[lid="${lid}""]{
+$("style:eq(0)").append(`#upro .light.fl.pro.break[lid="${lid}""]{
     background-image: url(https://up6.cc/2023/12/170224184664141.png);
     background-size: 100%;
 }`);
