7c4c1454ac9020bd4bc47a872bc39e2f9895a259
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-17 23:27:36 +0300

    Update and rename flh71.js to flh72.js

--- flh71.js
+++
@@ -1,8 +0,0 @@
-$("style:eq(0)").append(`#d2 .roomh[onclick="lid="18c1bf3fb78xb2a85404-x4djf4f""]{
-background-size: 220% auto;border-radius: 50px !important;border: 1px solid #0000;
-}`);
-$("style:eq(0)").append(`#upro .light.fl.pro.break[onclick="lid="18c1bf3fb78xb2a85404-x4djf4f""]{
-background-image: url(https://up6.cc/2023/12/170161252999561.png); background-size: cover;border-radius: 50px !important;color: #ffffff;
-}`);
-$(`<style>
-@keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
