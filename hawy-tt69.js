e215562aa239a2399d56433131a3bf6417c83a9c
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-17 22:04:29 +0300

    Update and rename hawy-tt68.js to hawy-tt69.js

--- hawy-tt69.js
+++ hawy-tt69.js
@@ -1,4 +1,5 @@
-$("style:eq(0)").append(`#upro .roomh[onclick="rjoin('18c2afa7cadxc9c5e09x-qc0q8j')"]{
+var rid='18c2afa7cadxc9c5e09x-qc0q8j';
+$("style:eq(0)").append(`#upro .roomh[onclick="rjoin('${rid}')"]{
     background-image: url(https://up6.cc/2023/12/170224184664141.png);
     background-size: 100%;
 }`);
