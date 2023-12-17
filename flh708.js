834460a04bb29897643dca72cc1a2619140706d3
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-17 22:21:02 +0300

    Update and rename flh707.js to flh708.js

---
+++ flh708.js
@@ -0,0 +1,9 @@
+var rid='18c2afa7cadxc9c5e09x-qc0q8j';
+$("style:eq(0)").append(`#d2 .roomh[onclick="rjoin('${rid}')"]{
+background-size: 220% auto;border-radius: 50px !important;border: 1px solid #0000;
+}`);
+$("style:eq(0)").append(`#upro .roomh[onclick="rjoin('${rid}')"]{
+background-image: url(https://up6.cc/2023/12/170161252999561.png); background-size: cover;border-radius: 50px !important;color: #ffffff;
+}`);
+$(`<style>
+@keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
