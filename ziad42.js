b0d9ba9d448b054a9c39af17d0d6bcb66d72d6fe
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-19 00:06:22 +0300

    Update and rename ziad41.js to ziad42.js

--- ziad42.js
+++ ziad42.js
@@ -1,12 +1,12 @@
 $(`<style>
-.fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .fitimg.u-pic {
+.fl.hand.nosel.uzr.uhtml.lid189e461646axfc2de284-xruzv99 .fitimg.u-pic {
   border-radius: 100px !important;
   margin-top: 10px !important;
   height: 55px !important;
   margin-left: -5px !important;
   width: 55px;
 }
-.fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f {
+.fl.hand.nosel.uzr.uhtml.lid189e461646axfc2de284-xruzv99 {
     background-size: 100% ;
     border-radius: 15px 0px 15px 0px;border: 1px solid #424242;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;
     background-image: url(https://up6.cc/2023/12/170276167205011.png);
@@ -15,14 +15,14 @@ $(`<style>
     margin-top: 2px!important;
     margin-bottom: 2px!important;
     }
-.fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .mini.u-msg {
+.fl.hand.nosel.uzr.uhtml.lid189e461646axfc2de284-xruzv99 .mini.u-msg {
     text-align: center;
     background-image: url(https://up6.cc/2023/12/170274954735011.gif);margin-right: 3px!important;
     margin-left: 2px!important;
     margin-top: 10px!important;
     margin-bottom: 18px!important;-webkit-background-clip: text;-webkit-text-fill-color: transparent;display: inline-block;background-size: 100%;
     }
-.uzr.d-flex.mm.lid18c1bf3fb78xb2a85404-x4djf4f {
+.uzr.d-flex.mm.lid189e461646axfc2de284-xruzv99 {
     border-radius: 5px 5px 5px 5px;
     margin-top: 2px!important;
     margin-bottom: 2px!important;
@@ -30,14 +30,14 @@ $(`<style>
     border: 0.5px solid #787878;
     box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #acacac;
     }
-.uzr.d-flex.mm.lid18c1bf3fb78xb2a85404-x4djf4f .fitimg.u-pic {border-radius: 15px 0px 15px 0px;border: 1px solid #ffffff;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;background-size: 100%;
+.uzr.d-flex.mm.lid189e461646axfc2de284-xruzv99 .fitimg.u-pic {border-radius: 15px 0px 15px 0px;border: 1px solid #ffffff;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;background-size: 100%;
 margin-bottom: 2px!important;
 }
-.uzr.d-flex.mm.pmsgc.lid18c1bf3fb78xb2a85404-x4djf4f {background-size: 100%;border-radius: 5px 5px 5px 5px;border: 2px solid #424242;background-image: url(https://up6.cc/2023/12/170276269397191.png);
+.uzr.d-flex.mm.pmsgc.lid189e461646axfc2de284-xruzv99 {background-size: 100%;border-radius: 5px 5px 5px 5px;border: 2px solid #424242;background-image: url(https://up6.cc/2023/12/170276269397191.png);
 }
-.uzr.d-flex.mm.pmsgc.lid18c1bf3fb78xb2a85404-x4djf4f .u-msg.break {text-align: center;background-image: url(https://up6.cc/2023/12/17027640111581.gif);-webkit-background-clip: text;-webkit-text-fill-color: transparent;display: inline-block;
+.uzr.d-flex.mm.pmsgc.lid189e461646axfc2de284-xruzv99 .u-msg.break {text-align: center;background-image: url(https://up6.cc/2023/12/17027640111581.gif);-webkit-background-clip: text;display: inline-block;
 }
-.fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .ustat {
+.fl.hand.nosel.uzr.uhtml.lid189e461646axfc2de284-xruzv99 .ustat {
 width: 12px !important;
 border-radius: 5px 0px 5px 0px;
 height: 12px !important;
@@ -49,7 +49,7 @@ box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #424242;
 }
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
 $("style").last().append(`
-.fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .ziad_i6ar1 {
+.fl.hand.nosel.uzr.uhtml.lid189e461646axfc2de284-xruzv99 .ziad_i6ar1 {
 width: 90px;
 margin-top: -15px;
 height: 88px;
@@ -57,4 +57,4 @@ margin-left: -17px;
 background-size: 100%;
 }
 `);
-$(".fl.hand.nosel.uzr.uhtml.lid18c1bf3fb78xb2a85404-x4djf4f .fitimg.u-pic").append('<img class="ziad_i6ar1" src="https://up6.cc/2023/12/170274593610761.gif">');
+$(".fl.hand.nosel.uzr.uhtml.lid189e461646axfc2de284-xruzv99 .fitimg.u-pic").append('<img class="ziad_i6ar1" src="https://up6.cc/2023/12/170274593610761.gif">');
