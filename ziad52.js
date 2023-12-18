ec74570b7936c86ddbc79625d0835057d3348117
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-19 01:42:28 +0300

    Update and rename ziad51.js to ziad52.js

--- ziad52.js
+++ ziad52.js
@@ -51,7 +51,7 @@ margin-bottom: 2px!important;
 }
 .fl.nosel.fa.fa-bullhorn {
     margin-top: 2px;
-    color: #0000ff;
+    color: #0000ff!important;
 }
 .fl.hand.nosel.uzr.uhtml.lid189e461646axfc2de284-xruzv99 .ustat {
 width: 12px !important;
