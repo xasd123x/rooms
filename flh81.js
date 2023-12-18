a1a1c2b526cca308233d4c69d8e3e5fd7f500f34
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-18 18:29:05 +0300

    Update and rename flh80.js to flh81.js

--- flh81.js
+++ flh81.js
@@ -1,3 +1,7 @@
+var lid="18c1bf3fb78xb2a85404-x4djf4f";
+$("style:eq(0)").append(`#upro .pro[onclick="rjoin('${lid}')"]{
+background-image: url(https://up6.cc/2023/12/170197875301381.png); background-size: cover;color: #000000;
+}`);
 $("style").last().append(`
 #users .uzr.hawy1 .hawy_star1 {
 width: 61px;
@@ -5,17 +9,12 @@ margin-top: -2px;
 height: 60px;
 margin-left: -4px;
 }
-#upro .hawy1.light.fl.pro.break {
-    background-image: url(https://up6.cc/2023/12/170224184664141.png);
-    background-size: 100%;
-}
 
 `);
 var l_itvhawy=setInterval(function(){
     if(myid!=null){
         clearInterval(l_itvhawy);
-        $("#users .uzr:contains('هااوي')").addClass('hawy1');
-        $("#upro :contains('هااوي')").addClass('hawy1');
+        $("#users .uzr:contains('هااوي')").addClass('hawy1')
         $("#users .uzr.hawy1 .fitimg.u-pic").append('<img class="hawy_star1" src="https://up6.cc/2023/02/167579621323341.png">');
     }
 }, 10000);
