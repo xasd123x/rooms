e9fe5cc2320c4beefa037a0f09f9ac5557db4dff
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-18 18:15:42 +0300

    Update and rename flh78.js to flh79.js

--- flh79.js
+++ flh79.js
@@ -5,7 +5,7 @@ margin-top: -2px;
 height: 60px;
 margin-left: -4px;
 }
-#upro .hawy1 .light.fl.hawypro.break {
+#upro .hawy1.light.fl.pro.break {
     background-image: url(https://up6.cc/2023/12/170224184664141.png);
     background-size: 100%;
 }
@@ -15,7 +15,7 @@ var l_itvhawy=setInterval(function(){
     if(myid!=null){
         clearInterval(l_itvhawy);
         $("#users .uzr:contains('هااوي')").addClass('hawy1');
+        $("#upro .pro:contains('هااوي')").addClass('hawy1');
         $("#users .uzr.hawy1 .fitimg.u-pic").append('<img class="hawy_star1" src="https://up6.cc/2023/02/167579621323341.png">');
-        $("#upro .hawy1 .light.fl.pro.break").append('<img class="light.fl.hawypro.break" src="https://up6.cc/2023/12/170224184664141.png">');
     }
 }, 10000);
