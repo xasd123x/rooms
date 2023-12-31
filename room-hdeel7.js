9adb3191463e93c1fe6c1149d5c2202fa38087ab
xasd123x <153305949+xasd123x@users.noreply.github.com>
2023-12-31 22:44:44 +0300

    Update and rename room-hdeel6.js to room-hdeel7.js

--- room-hdeel7.js
+++ room-hdeel7.js
@@ -205,7 +205,7 @@ border-radius:  10px 0 10px!important;
 }
 #upro.hdeel1 .light.fl.pro.break {
     background-image: linear-gradient(225deg,#dfdfdf 0%,#f7f7f7 29%,#f4f4f4 67%,#dbdbdb 100%);
-    background-image: url(https://up6.cc/2023/11/170008295237174.png);
+    background-image: url(https://up6.cc/2023/12/170405185067311.png);
     background-size: 100%;
 }
 #mic.hdeel1 #muteall {
