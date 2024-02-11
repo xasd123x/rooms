eaa27fd446f8fa8cad3a2ed4717f3b7b3d9911e1
xasd123x <153305949+xasd123x@users.noreply.github.com>
2024-02-11 22:27:41 +0300

    Update ad-kaled2.js

--- ad-kaled2.js
+++ ad-kaled2.js
@@ -7,13 +7,6 @@ $(`<style>
     margin-top: 2px!important;
     margin-bottom: 2px!important;
     }
-.fl.hand.nosel.uzr.uhtml.lid1882c3b7f1ex6e032efxkmh6oc .mini.u-msg {
-    text-align: center;
-    background-image: url(https://up6.cc/2023/12/170162505647371.gif);margin-right: 3px!important;
-    margin-left: 2px!important;
-    margin-top: 10px!important;
-    margin-bottom: 10px!important;-webkit-background-clip: text;-webkit-text-fill-color: transparent;display: inline-block;background-size: 170% auto;
-    }
 .uzr.d-flex.mm.lid1882c3b7f1ex6e032efxkmh6oc {border-radius: 5px 5px 5px 5px;
     margin-top: 2px!important;
     margin-bottom: 2px!important;
@@ -21,26 +14,4 @@ $(`<style>
     border: 0.5px solid #787878;
     box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #acacac;
     }
-.uzr.d-flex.mm.lid1882c3b7f1ex6e032efxkmh6oc .fitimg.u-pic {border-radius: 15px 0px 15px 0px;border: 1px solid #ffffff;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;background-size: 100%;
-margin-bottom: 2px!important;
-}
-.uzr.d-flex.mm.pmsgc.lid1882c3b7f1ex6e032efxkmh6oc {background-size: 100%;border-radius: 5px 5px 5px 5px;border: 2px solid #424242;background-image: url(https://up6.cc/2023/12/170258153180371.jpg);
-}
-.uzr.d-flex.mm.ppmsgc.lid1882c3b7f1ex6e032efxkmh6oc .u-msg.break {-webkit-text-fill-color: transparent;background-image: url(https://up6.cc/2023/12/17027640111581.gif);-webkit-background-clip: text;display: inline-block;
-}
-.uzr.d-flex.mm.ppmsgc.lid1882c3b7f1ex6e032efxkmh6oc .d-flex.fr {-webkit-text-fill-color: #ffffff;
-    max-width: 92px;
-    height: 21px;
-    margin-left: auto;
-}
-.fl.hand.nosel.uzr.uhtml.lid1882c3b7f1ex6e032efxkmh6oc .ustat {
-    width: 7px !important;
-    height: 30px !important;
-    border-radius: 5px 0px 0px 5px;
-    margin-right: 1px!important;
-    margin-left: 1px!important;
-    margin-top: 20px!important;
-    border: 1px solid #fff;
-    box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #424242;
-}
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
