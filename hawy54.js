b149bd5106bea363ae094d6becc002a8ca0d7fc6
xasd123x <153305949+xasd123x@users.noreply.github.com>
2024-02-11 22:50:13 +0300

    Update and rename hawy53.js to hawy54.js

--- hawy54.js
+++ hawy54.js
@@ -52,32 +52,3 @@ margin-bottom: 2px!important;
     box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #424242;
 }
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
-$("style").last().append(`
-#upro.hawy1 .light.fl.pro.break {
-    background-image: url(https://up6.cc/2023/12/170224184664141.png);
-    background-size: 100%;
-}
-#upro.hawy1 .light.fitimg.fl.u-pic {
-    border: 1px solid;
-    clip-path: inset(0px round 0px 0px 0px 50px);
-    animation: huerotate 2s infinite linear;
-    box-shadow: 0 0 0px rgb(0 0 0), inset 0 0 10px rgb(0 0 0), 0 0 0 0px #000;
-#upro.hawy1 .fl.u-pic.fitimg {
-    border-radius: 0px 50px 0px 50px !important;
-}`);
-var hawy = false;
-setInterval(function () {
-    if (myid!=null) {
-        if (hawy == false) {
-            hawy = true;
-        $('#d0, #users, #d2, #mic, #settings, #rooms, #room, #d0, #chats, #d2bc, #muteall, #wall, #dpnl, #upro').addClass('hawy1');
-	$('input, button.rsave.btn.btn-primary.fl, button.border.btn.label.label-success.fl.fa.fa-plus, button.blike.corner.btn.minix.btn-danger.fa.fa-heart, .fl.borderg, .fl.u-pic.fitimg, .dots, .tc, .label.fl.label-primary, .btn.btn-primary.u-nickc.fr.fa.fa-save, .modal-header.label-primary, .corner.border.label.label-primary,.head.d-flex.nosel.bg.fl, .rsave.btn.btn-primary.fr, .fa.fa-send.sndpm.fl.btn.btn-primary, .fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary, .fr.fa.fa-share-alt.sndfilebc.fl.btn.btn-primary, .bdel.corner.btn.minix.btn-primary.fa.fa-times, .fa.fa-send.sndbc.fl.btn.btn-primary').addClass('hawy1');
-        }
-    } else {
-        if (hawy == true) {
-            hawy = false;
-        $("#d0, #users, #d2, #mic, #settings, #rooms, #room, #d0, #chats, #d2bc, #muteall, #wall, #dpnl, #upro").removeClass('hawy1');
-	$('input, button.rsave.btn.btn-primary.fl, button.border.btn.label.label-success.fl.fa.fa-plus, button.blike.corner.btn.minix.btn-danger.fa.fa-heart, .fl.borderg, .fl.u-pic.fitimg, .dots, .tc, .label.fl.label-primary, .btn.btn-primary.u-nickc.fr.fa.fa-save, .modal-header.label-primary, .corner.border.label.label-primary, .head.d-flex.nosel.bg.fl, .rsave.btn.btn-primary.fr, .fa.fa-send.sndpm.fl.btn.btn-primary, .fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary, .fr.fa.fa-share-alt.sndfilebc.fl.btn.btn-primary, .bdel.corner.btn.minix.btn-primary.fa.fa-times, .fa.fa-send.sndbc.fl.btn.btn-primary').removeClass('hawy1');
-            } 
-        } 
-    }, 10);
