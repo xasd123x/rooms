088f32dee01a3b8dfc286df8089f9cac8e5cc0ea
xasd123x <153305949+xasd123x@users.noreply.github.com>
2024-02-11 22:31:44 +0300

    Update and rename ad-kaled4.js to ad-kaled5.js

--- ad-kaled5.js
+++ ad-kaled5.js
@@ -20,7 +20,7 @@ $(`<style>
     border-radius: 5px 0px 0px 5px;
     margin-right: 1px!important;
     margin-left: 1px!important;
-    margin-top: 20px!important;
+    margin-top: 10px!important;
     border: 1px solid #fff;
     box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #424242;
 }
