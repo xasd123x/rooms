8dfe203a0e76743fb0f3eb42850f3f665a4ed916
xasd123x <153305949+xasd123x@users.noreply.github.com>
2024-02-11 22:30:05 +0300

    Update and rename ad-kaled3.js to ad-kaled4.js

--- ad-kaled4.js
+++ ad-kaled4.js
@@ -14,4 +14,14 @@ $(`<style>
     border: 0.5px solid #787878;
     box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #acacac;
     }
+    .fl.hand.nosel.uzr.uhtml.lid1882c3b7f1ex6e032efxkmh6oc .ustat {
+    width: 7px !important;
+    height: 30px !important;
+    border-radius: 5px 0px 0px 5px;
+    margin-right: 1px!important;
+    margin-left: 1px!important;
+    margin-top: 20px!important;
+    border: 1px solid #fff;
+    box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #424242;
+}
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
