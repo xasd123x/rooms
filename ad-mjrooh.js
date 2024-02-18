0881e70b6cc71f242e756f05b68cda51992433b9
xasd123x <153305949+xasd123x@users.noreply.github.com>
2024-02-18 18:04:56 +0300

    Create ad-mjrooh.js

---
+++ ad-mjrooh.js
@@ -0,0 +1,27 @@
+$(`<style>
+.fl.hand.nosel.uzr.uhtml.lid17f4e873fedx912d8066-xe3r9qy {
+    background-size: 100%;
+    border-radius: 15px 0px 15px 0px;border: 1px solid #424242;box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 3px #424242;
+    margin-right: 0px!important;
+    margin-left: 0px!important;
+    margin-top: 2px!important;
+    margin-bottom: 2px!important;
+    }
+.uzr.d-flex.mm.lid17f4e873fedx912d8066-xe3r9qy {border-radius: 5px 5px 5px 5px;
+    margin-top: 2px!important;
+    margin-bottom: 2px!important;
+    border-bottom: 0.5px solid #787878!important;
+    border: 0.5px solid #787878;
+    box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #acacac;
+    }
+    .fl.hand.nosel.uzr.uhtml.lid17f4e873fedx912d8066-xe3r9qy .ustat {
+    width: 7px !important;
+    height: 40px !important;
+    border-radius: 5px 0px 0px 0px;
+    margin-right: 1px!important;
+    margin-left: 1px!important;
+    margin-top: 10px!important;
+    border: 1px solid #fff;
+    box-shadow: inset 0 0 0 rgb(0 0 0 / 8%), 0 0 2px #424242;
+}
+@keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
